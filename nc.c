59d872937689223a97e43fc3a8cc29f2cfd39d93
VENOM-InstantDeath <mohathepro111@gmail.com>
2024-03-11 00:58:44 -0300

    Updated the library

--- nc.c
+++ nc.c
@@ -2,10 +2,29 @@
 #include "nc.h"
 #include "../libvector/vector.h"
 
+void string_append_char_at(string* S, int index, char ch) {
+	size_t end = S->memsize;
+	string_grow(S, 1);
+	index = index < 0 ? S->memsize+index : index;
+	if (S->memsize > 1) string_shift(S, index, end, 1);
+	string_asign_at(S, index, ch);
+}
+
+void string_pop_at(string* S, int index) {
+	index = index < 0 ? S->memsize+index : index;
+	if (S->memsize > 1) string_shift_left(S, index+1, S->memsize, 1);
+	string_shrink(S, 1);
+}
+
+void clear_region(WINDOW* win, int y, int x, int n) {
+	for (;x<n; x++) {mvwaddch(win, y, x, ' ');}
+}
+
 int Sread(WINDOW* win, int y, int x, int visual_limit, int char_limit, char** buff) {
 	string S = string_init(*buff);
 	curs_set(1);
 	int p[2]={0};
+	int scrolln=0;
 	for (;;) {
 		int ch = mvwgetch(win, y, x+p[0]);
 		switch (ch) {
@@ -13,10 +32,41 @@ int Sread(WINDOW* win, int y, int x, int visual_limit, int char_limit, char** bu
 				curs_set(0);
 				return 0;
 			case KEY_LEFT:
+				if (!p[1]) continue;
+				if (!p[0]) {
+					clear_region(win, y, x, x+visual_limit);
+					scrolln--;
+					mvwaddnstr(win, y, x, S.c_str+scrolln, visual_limit);
+					p[1]--;
+					continue;
+				}
+				p[0]--; p[1]--;
 				break;
 			case KEY_RIGHT:
+				if (p[1] == S.memsize || p[1]==char_limit) continue;
+				if (p[0] == visual_limit) {
+					clear_region(win, y, x, x+visual_limit);
+					scrolln++;
+					mvwaddnstr(win, y, x, S.c_str+scrolln, visual_limit);
+					p[1]++;
+					continue;
+				}
+				p[0]++; p[1]++;
 				break;
+			case 127:
 			case KEY_BACKSPACE:
+				if (!p[1]) continue;	
+				string_pop_at(&S, p[1]-1);
+				if (!p[0]) {
+					clear_region(win, y, x, x+visual_limit);
+					scrolln--;
+					mvwaddnstr(win, y, x, S.c_str+scrolln, visual_limit);
+					p[1]--;
+				} else {
+					p[0]--; p[1]--;	
+					clear_region(win, y, x+p[0], x+visual_limit);
+					mvwaddnstr(win, y, x+p[0], S.c_str+p[1], S.memsize>visual_limit ? visual_limit+scrolln-p[1] : S.memsize-p[1]);
+				}
 				break;
 			case 4:
 			case 10:
@@ -24,12 +74,20 @@ int Sread(WINDOW* win, int y, int x, int visual_limit, int char_limit, char** bu
 				*buff = string_get_c_str(&S);
 				return 1;
 			default:
-				if (p[1] == char_limit) continue; // IF p[0] IS VISUAL_LIMIT SCROLL HORIZONTALLY
+				if (S.memsize == char_limit) continue; // IF p[0] IS VISUAL_LIMIT SCROLL HORIZONTALLY
 				if (!((ch >= ' ' && ch <= '~') || (ch >= 225 && ch <= 250))) continue;
-				string_append_char(&S, ch);
-				mvwaddch(win, y, x+p[0], ch);
-				p[0]++; p[1]++;
-				break;
+				string_append_char_at(&S, p[1],ch);
+				if (p[0] == visual_limit) {
+					clear_region(win, y, x, x+visual_limit);
+					scrolln++;
+					mvwaddnstr(win, y, x, S.c_str+scrolln, visual_limit);
+					p[1]++;
+					continue;
+				} else {
+					clear_region(win, y, x+p[0], x+visual_limit);
+					mvwaddnstr(win, y, x+p[0], S.c_str+p[1], S.memsize>visual_limit ? visual_limit+scrolln-p[1] : S.memsize-p[1]);
+					p[0]++; p[1]++;
+				}
 		}
 	}
 	return 1;
--- test.c
+++ test.c
@@ -5,14 +5,16 @@ int main() {
 	WINDOW* stdscr = initscr();
 	curs_set(0); keypad(stdscr, 1);
 	start_color(); use_default_colors();
-	cbreak();
+	cbreak(); noecho();
 	init_pair(1, 20, 15);
 	int y, x; getmaxyx(stdscr, y, x);
 	WINDOW* win = newwin(3, 50, y/2-1, x/2-25);
+	keypad(win, 1);
 	wbkgd(win, COLOR_PAIR(1));
 	wrefresh(stdscr); wrefresh(win);
-	char* buff;
-	int err = Sread(win, 1, 2, 20, 20, &buff); 
+	mvwaddch(win, 1, 23, 'x');
+	char* buff=NULL;
+	int err = Sread(win, 1, 2, 20, 30, &buff); 
 	endwin();
-	puts(buff);
+	if (err) puts(buff);
 }
